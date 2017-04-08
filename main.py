478981212c4ecebcf129db356fd2f016a1822d70
Appelman <jpkotze1@gmail.com>
2017-04-09 01:24:46 +0200

    added dummy code to read temp from openweather

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import openweather.domains.city as city
 import openweather.domains.wind as wind
 import openweather.domains.temperatureReading as tempReading
+import openweather.infrastructure.repositories.openWeatherRepository as openWeatherRepository
+import json
+import urllib.request
 
-myTempReading = tempReading.TemperatureReading()
-myTempReading.city.name = "temp city"
+#request = http.Request("api.openweathermap.org/data/2.5/weather?q=Eden Glen,ZA&appid=b50e52c1dd9315a140918d576959dabe")
+#with http.urlopen(req) as response:
+#    result = response.readall().decode("utf-8")
 
-print(myTempReading.city.name)
+req = urllib.request.Request('http://api.openweathermap.org/data/2.5/weather?q=Eden Glen,ZA&appid=b50e52c1dd9315a140918d576959dabe&units=metric')
+with urllib.request.urlopen(req) as response:
+    result = json.loads(response.readall().decode('utf-8'))
+
+repo = openWeatherRepository.OpenweatherRepository()
+
+print(repo.read())
---
+++ openweather/infrastructure/repositories/openWeatherRepository.py
@@ -0,0 +1,17 @@
+import openweather.core.baseRepository as baseRepository
+
+class OpenweatherRepository(baseRepository.BaseRepository):
+    def __init__(self):
+        pass
+
+    def read(self):
+        return []
+    
+    def create(self):
+        pass
+    
+    def update(self):
+        pass
+    
+    def delete(self):
+        pass
