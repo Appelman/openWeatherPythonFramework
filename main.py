89b3cbacccdb294133f2eaab7e6286b0115cbdb5
Appelman <jpkotze1@gmail.com>
2017-04-10 08:16:18 +0200

    Tested api request

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import openweather.domains.wind as wind
 import openweather.domains.temperatureReading as tempReading
 import openweather.infrastructure.repositories.openWeatherRepository as openWeatherRepository
 import json
-import urllib.request
+import urllib.request 
 
 #request = http.Request("api.openweathermap.org/data/2.5/weather?q=Eden Glen,ZA&appid=b50e52c1dd9315a140918d576959dabe")
 #with http.urlopen(req) as response:
